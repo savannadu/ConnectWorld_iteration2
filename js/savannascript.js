4df5c08a7d1144041aa5c258247a7fccb4167b79
savannadu <xue.du.2013@sis.smu.edu.sg>
2015-10-11 17:43:06 +0800

    Sign in and forget your password done

--- js/savannascript.js
+++ js/savannascript.js
@@ -2,7 +2,7 @@
  * Created by Savanna on 7/10/2015.
  */
 var connectWorld = angular.module('connectWorld',[]);
-connectWorld.controller('myProfileController', function ($scope) {
+connectWorld.controller('myProfileController', ['$scope', function ($scope) {
     $scope.basicInfo =
     {
         "name": "Jimmy Goh",
@@ -37,49 +37,26 @@ connectWorld.controller('myProfileController', function ($scope) {
         document.getElementById(idHide).style.display = "inline";
         document.getElementById(idDisplay).style.display = "none";
     };
-});
 
-connectWorld.controller('editMyProfileController', function ($scope,$http) {
-    $scope.basicInfo =
-    {
-        "name": "Jimmy Goh",
-        "profilePic": "../img/buddyProfilePic.png",
-        "gender": "Male",
-        "birthday": new Date('1989', '10', '10'),
-        "nationality": "Singaporean",
-        "location": "North East, Singapore",
-        "mySelf": "I am pursuing a Master degree programme in Business Administration at Japan Tokyo University. I would like to improve my Japanese so that I will be able to integrate and adapt into the Japanese community as soon as possible."
+    $scope.update = function (basicInfo, language, interestTags) {
+        $scope.basicInfo = angular.copy(basicInfo);
+        $scope.langugae = angular.copy(language);
+        $scope.master = angular.copy(interestTags);
+
     };
-    $scope.language =
-    {
-        "proficient": "English, Mandarine",
-        "interested": "Japanese, French"
-    };
-    $scope.interestTags = [
-        {"tag": "Cooking"},
-        {"tag": "Travel"},
-        {"tag": "Animie"},
-        {"tag": "Pokemon"}
-    ];
-    $scope.checkGender = function(data) {
-        if (data !== 'FEMALE' && data !== 'MALE') {
-            return "Username should be 'Female' or 'Male'";
+
+    $scope.reset = function (form) {
+        if (form) {
+            form.$setPristine();
+            form.$setUntouched();
         }
+        $scope.basicInfo = angular.copy(basicInfo);
+        $scope.langugae = angular.copy(language);
+        $scope.master = angular.copy(interestTags);
     };
 
-    $scope.saveUser = function() {
-        // $scope.user already updated!
-        return $http.post('/saveUser', $scope.user).error(function(err) {
-            if(err.field && err.msg) {
-                // err like {field: "name", msg: "Server-side error for this username!"}
-                $scope.editableForm.$setError(err.field, err.msg);
-            } else {
-                // unknown error
-                $scope.editableForm.$setError('name', 'Unknown error!');
-            }
-        });
-    };
-});
+    $scope.reset();
+}]);
 /*
 connectWorld.run(function (editableOptions) {
     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
