fea452ab84ac964d0aa745ac61673910524f1e37
stellareddy <s.tellaaa@hotmail.com>
2015-10-14 12:12:48 +0800

    Display of chat content
    input box not working at the moment

--- js/stellaDB.js
+++ js/stellaDB.js
@@ -7,7 +7,9 @@ var userProfiles = [
   ["Stella", "20", "Female", ["English", "Chinese"], "img/greyprofilepic.png"],
   ["John", "40", "Male", ["Malay", "Chinese"], "img/greyprofilepic.png"],
   ["Leo", "25", "Male", ["English", "Chinese", "Cantonese"], "img/greyprofilepic.png"],
-  ["Shane", "25", "Male", ["English", "Hokkien"], "img/greyprofilepic.png"]
+  ["Shane", "25", "Male", ["English", "Hokkien"], "img/greyprofilepic.png"],
+  ["Sora", "25", "Male", ["English", "Hokkien"], "img/greyprofilepic.png"],
+  ["Timmy", "25", "Male", ["English", "Hokkien"], "img/greyprofilepic.png"]
 ];
 
 
@@ -50,17 +52,18 @@ var commentSora  = [
     [1,"Hi Timmy, I love cafehopping too! But im not familiar with the roads in Singapore hoping to make a friend to hang out with</br> I can help you with Jap and probably you bring me around Singapore? </br> Hope to hear from you soon."],
     [0,"Hi Sora, wow! You're from Japan! What are you doing here in Singapore?"]
 ];
-var commentBuddy = [
-    [0,"Hi XXX are you coming to the event later on tonight?"],
+var commentTinna = [
+    [0,"Hi Tinna, are you coming to the event later on tonight?"],
     [1,"Hi Timmy, yeah, but I'm not familiar with the location"],
     [0,"No worries, let's meet at City Hall MRT @ 5 later then!"],
     [1,"&#30906;&#20449;&#12375;&#12390;"]
 ];
 // Converter http://mylanguages.org/converter.php
 
+// id chat with id, which chat array
 var commentDB = [
-    ["XiaoMing","img/greyprofilepic.png",commentBuddy],
-    ["Sora","img/redprofilepic.png",commentSora]
+    [9, 0,commentTinna],
+    [9, 8,commentSora]
 ];
 
 
